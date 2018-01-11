9a849fb253e8259323959bbb94e39536b8345292
chad-tung <chad.tung@gmail.com>
2018-01-11 01:52:52 +0000

    Double filter works, but is delayed

--- g3_makeup_selector/src/containers/MakeupBox.js
+++ g3_makeup_selector/src/containers/MakeupBox.js
@@ -13,7 +13,7 @@ class MakeupBox extends React.Component {
     this.handleSelectedType = this.handleSelectedType.bind(this);
     // this.brandFilter = this.brandFilter.bind(this);
     // this.typeFilter = this.typeFilter.bind(this);
-    // this.handleChange = this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -43,30 +43,50 @@ class MakeupBox extends React.Component {
       }
     })
     xhr.send();
+    this.handleChange();
   }
 
   handleSelectedBrand(brandName) {
     if (brandName == "all") {
       this.setState({filterBrand: null})
-      this.setState({displayedProducts: this.state.products})
+      // this.setState({displayedProducts: this.state.products})
     } else {
       this.setState({filterBrand: brandName})
-      const filteredArr = this.state.products.filter(product => product.brand == brandName)
-      this.setState({displayedProducts: filteredArr});
+      // const filteredArr = this.state.products.filter(product => product.brand == brandName)
+      // this.setState({displayedProducts: filteredArr});
     }
-    // this.handleChange();
+    this.handleChange();
   }
 
   handleSelectedType(typeName) {
     if (typeName == "all") {
       this.setState({filterType: null})
-      this.setState({displayedProducts: this.state.products})
+      // this.setState({displayedProducts: this.state.products})
     } else {
       this.setState({filterType: typeName})
-      const filteredArr = this.state.products.filter(product => product.product_type == typeName)
+      // const filteredArr = this.state.products.filter(product => product.product_type == typeName)
+      // this.setState({displayedProducts: filteredArr});
+    }
+    this.handleChange();
+  }
+
+  handleChange() {
+    const typefilter = this.state.filterType;
+    const brandfilter = this.state.filterBrand;
+    let filteredArr = [];
+
+    if (!brandfilter) {
+      filteredArr = this.state.products;
+    } else {
+      filteredArr = this.state.products.filter(product => product.brand == brandfilter);
+    }
+
+    if (!typefilter) {
+      this.setState({displayedProducts: filteredArr});
+    } else {
+      filteredArr = filteredArr.filter(product => product.product_type == typefilter);
       this.setState({displayedProducts: filteredArr});
     }
-    // this.handleChange();
   }
   //
   // handleChange() {
